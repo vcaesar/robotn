a2f41f03b5ac8e4e1db4b536805e8929859a6e0a
vcaesar <vzvway@gmail.com>
2019-04-05 13:33:06 -0400

    update code style

--- lua/robot.lua
+++ lua/robot.lua
@@ -1,6 +1,16 @@
+-- // Copyright 2016 The go-vgo Project Developers. See the COPYRIGHT
+-- // file at the top-level directory of this distribution and at
+-- // https://github.com/go-vgo/robotgo/blob/master/LICENSE
+-- //
+-- // Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+-- // http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+-- // <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+-- // option. This file may not be copied, modified, or distributed
+-- // except according to those terms.
+
 local ffi = require("ffi")
 local robot = ffi.load("../robotgo")
 
 ffi.cdef([[
-extern char* GetPixelColor(GoInt x, GoInt y);
+    extern char* GetPixelColor(GoInt x, GoInt y);
 ]]);
